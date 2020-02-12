4b04a2402198c0d19f012d6fb3e3a04d69f027c5
octaviandd <octaviandd@yahoo.com>
2020-02-12 22:48:05 +0000

    update4

--- src/RecipesAPI.js
+++ src/RecipesAPI.js
@@ -5,17 +5,15 @@ const axios = require("axios");
 const recipesRequest = param => {
   let api_key = "5afc892716085e550976cd29a6843beb 	";
   let api_id = "697b97d8";
-  return fetch(
-    `https://api.edamam.com/search/q=${param}&app_id=${api_id}&app_key=${api_key}`,
-    {
-      headers: {
-        "Access-Control-Allow-Origin": "*"
-      }
-    }
-  ).then(res => {
-    console.log(res.json());
-    return res.json();
-  });
+  const proxyurl = "https://cors-anywhere.herokuapp.com/";
+  const url = `https://api.edamam.com/search/q=chicken&app_id=${api_id}&app_key=${api_key}`;
+  return fetch(proxyurl + url)
+    .then(res => {
+      return res.json();
+    })
+    .then(function(data) {
+      console.log(data);
+    });
 };
 
 export default recipesRequest;
