cddfce8955f9150f25d6cba3371fff7f82b58cc0
octaviandd <octaviandd@yahoo.com>
2020-02-16 19:02:54 +0000

    update

--- src/RecipesAPI.js
+++ src/RecipesAPI.js
@@ -3,12 +3,12 @@
 const axios = require("axios");
 
 const recipesRequest = param => {
-  let api_key = "b2173a8c45c3e825d54308d82f82cc21";
-  let api_id = "eff5e213";
+  let api_key = "70449e4d68b24be9fb1f9d4da3b7cb72";
+  let api_id = "8540b488";
 
   return axios
     .get(
-      `https://api.edamam.com/search?q=chicken&app_id=${api_id}&app_key=${api_key}`,
+      `https://api.edamam.com/search?q=${param}&app_id=${api_id}&app_key=${api_key}`,
       {
         headers: {
           "Content-Type": "application/json"
